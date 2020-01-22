423356e85990a0f88b6d33e93475fa42406e1474
Stephen <stevoschool@gmail.com>
2020-01-22 14:35:21 -0500

    inclass work. Created new tests

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -25,6 +25,26 @@ class BankAccountTest {
     void isEmailValidTest(){
         assertTrue(BankAccount.isEmailValid( "a@b.com"));
         assertFalse( BankAccount.isEmailValid(""));
+
+        assertFalse(BankAccount.isEmailValid("a..b@male.com"));
+        assertFalse(BankAccount.isEmailValid("abc-@mail.com"));
+        assertFalse(BankAccount.isEmailValid(".abc@mail.com"));
+        assertFalse(BankAccount.isEmailValid("abc#def@mail.com"));
+
+        assertTrue(BankAccount.isEmailValid("abc-d@mail.com"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com"));
+        assertTrue(BankAccount.isEmailValid("abc@mail.com"));
+        assertTrue(BankAccount.isEmailValid("abc_def@mail.com"));
+
+        assertFalse(BankAccount.isEmailValid("abc.def@mail.c"));
+        assertFalse(BankAccount.isEmailValid("abc.def@mail#archive.com"));
+        assertFalse(BankAccount.isEmailValid("bc.def@mail"));
+        assertFalse(BankAccount.isEmailValid("abc.def@mail..com"));
+
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.cc"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail-archive.com"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.org"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com"));
     }
 
     @Test
