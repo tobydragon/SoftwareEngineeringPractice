11bac5e59c54cd1594e05da57d3fe3170ef35c2e
coconnell1 <coconnell1@ithaca.edu>
2020-01-23 23:51:00 -0500

    Equivalence and border cases

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -8,39 +8,95 @@ public class BankAccount {
     /**
      * @throws IllegalArgumentException if email is invalid
      */
-    public BankAccount(String email, double startingBalance){
-        if (isEmailValid(email)){
+    public BankAccount(String email, double startingBalance) {
+        if (isEmailValid(email)) {
             this.email = email;
             this.balance = startingBalance;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
         }
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
-    public void withdraw (double amount)  {
+    public void withdraw(double amount) {
         balance -= amount;
 
     }
 
 
-    public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+    public static boolean isEmailValid(String email) {
+        String validDigits = "-_@.";
+
+        //number of @ symbols
+        int atCount = 0;
+
+        //iterates each character of email
+        for (int i = 0; i < email.length() - 1; i++) {
+            Character c = email.charAt(i);
+
+            //checks number of @s
+            if (c == validDigits.charAt(2)) {
+                atCount++;
+            }
+            //checks that each character is a valid digit
+            if (!(Character.isLetter(c) || Character.isDigit(c) || c == validDigits.charAt(0) || c == validDigits.charAt(1) || c == validDigits.charAt(2) || c == validDigits.charAt(3))) {
+                return false;
+
+                //if current character is an allowed symbol, it checks that it is followed by a letter or number
+            } else if (c == validDigits.charAt(0) || c == validDigits.charAt(1) || c == validDigits.charAt(2) || c == validDigits.charAt(3)) {
+                Character nextChar = email.charAt(i + 1);
+                if (!(Character.isLetter(nextChar) || Character.isDigit(nextChar))) {
+                    return false;
+                }
+            }
+        }
+        //checks if email is empty
+        if (email.isEmpty()) {
             return false;
         }
-        else {
+
+        //split the email at the @ so we can only look at domain
+        String[] emailSplit = email.split("@");
+        String domain = emailSplit[1];
+        int periodIndex = 1000;
+        String period = ".";
+
+        //iterates domain to find where the period is
+        for (int x = 0; x < domain.length() - 1; x++) {
+            Character c2 = domain.charAt(x);
+            if (c2 == period.charAt(0)) {
+                periodIndex = x;
+            }
+        }
+        //make sure domain contains a .
+        if (!domain.contains(".")) {
+            return false;
+
+            //makes there is atleast two characters after period in domain
+        } else if (domain.length() - 1 - periodIndex < 2) {
+            return false;
+
+            //checks that first character is a letter
+        } else if (!Character.isLetter(email.charAt(0))) {
+            return false;
+
+            //checks if that there is exactly one @ symbol
+        } else if (atCount != 1) {
+            return false;
+        } else {
             return true;
         }
+
+        }
     }
-}
+
--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -18,31 +18,74 @@ class BankAccountTest {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
 
+        /**for this function, the only two equivalence classes would be withdrawing more than you have
+         (giving expected negative balance) and having enough in your balance to withdraw (expected positive).
+         This assert test would be apart of the equivalence class that is valid, and it is not a border case.
+         There is not an equivalence class for invalid withdrawals (that would give you a negative balance or attempting to withdraw a negative amount) and there is not a border case
+         to test withdrawing how much you have. The only border case there should be is withdrawing how much the current balance is and withdrawing 0, because we did
+         not specify a maximum withdrawal.
+         */
         assertEquals(100, bankAccount.getBalance());
     }
 
     @Test
     void isEmailValidTest(){
         //Valid Checks
+        //I am confused on how equivalence classes and borders would be applied to emails
+
+        //equivalence class of valid email with no symbols and correct domain. border case because minimum digits before domain
         assertTrue(BankAccount.isEmailValid( "a@b.com"));
+
+        //equivalence class of valid email with a symbol and a letter following. border case for letters/digit follow symbol
         assertTrue(BankAccount.isEmailValid( "abc-d@mail.com"));
+
+        //same equivalence class as above, not border case
         assertTrue(BankAccount.isEmailValid( "abc.def@mail.com"));
+
+        //same equivalence class as first test, not border case
         assertTrue(BankAccount.isEmailValid( "abc@mail.com"));
+
+        //same class as second test, not border case
         assertTrue(BankAccount.isEmailValid( "abc_def@mail.com"));
+
+        //same equivalence class as second test but also border case because of the length of .cc
         assertTrue(BankAccount.isEmailValid( "abc.def@mail.cc"));
+
+        //equivalence class of symbol in both prefix and domain, not border case
         assertTrue(BankAccount.isEmailValid( "abc.def@mail-archive.com"));
+
+        //same class as second,not border case
         assertTrue(BankAccount.isEmailValid( "abc.def@mail.org\n"));
+
+        //same class as second, not border case
         assertTrue(BankAccount.isEmailValid( "abc.def@mail.com"));
 
         //Invalid Checks
+        //border case and equivalence class of length
         assertFalse( BankAccount.isEmailValid(""));
+
+        //equivalence class of invalid email because symbol not followed by letter or digit. border case for same reason
         assertFalse( BankAccount.isEmailValid("abc-@mail.com"));
+
+        //equivalence class of invalid email because back to back symbols. not border case
         assertFalse( BankAccount.isEmailValid("abc..def@mail.com"));
+
+        //equivalence class of invalid email because not starting with a letter. not border case
         assertFalse( BankAccount.isEmailValid(".abc@mail.com"));
+
+        //equivalence class of invalid symbol. not border case
         assertFalse( BankAccount.isEmailValid("abc#def@mail.com"));
+
+        //equivalence class of too short of domain after the period, not border case
         assertFalse( BankAccount.isEmailValid("abc.def@mail.c"));
+
+        //equivalence class of having no @ for domain. not border case
         assertFalse( BankAccount.isEmailValid("abc.def@mail#archive.com"));
+
+        //equivalence class of not having a . in domain. not border case
         assertFalse( BankAccount.isEmailValid("abc.def@mail"));
+
+        //equivalence class of having two . in domain. not border case
         assertFalse( BankAccount.isEmailValid("abc.def@mail..com"));
     }
 
