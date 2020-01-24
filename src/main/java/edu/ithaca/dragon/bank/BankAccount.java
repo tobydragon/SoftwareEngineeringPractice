1fd703263a0ed1e1069f718413242168b35ccc0d
Matthew Becker <mbecker5@ithaca.edu>
2020-01-24 08:00:22 -0500

    Added implementation based on the test

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -8,37 +8,48 @@ public class BankAccount {
     /**
      * @throws IllegalArgumentException if email is invalid
      */
-    public BankAccount(String email, double startingBalance){
-        if (isEmailValid(email)){
+    public BankAccount(String email, double startingBalance) {
+        if (isEmailValid(email)) {
             this.email = email;
             this.balance = startingBalance;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
         }
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
-    public void withdraw (double amount)  {
+    public void withdraw(double amount) {
         balance -= amount;
 
     }
 
 
-    public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+    public static boolean isEmailValid(String email) {
+        if (email.indexOf('@') == -1) {
             return false;
         }
+        if (email.startsWith(".")) {
+            return false;
+        }
+
+        if (!email.endsWith(".com")) {
+            return false;
+        }
+
+        if(email.contains("-@")){
+            return false;
+        }
+
         else {
             return true;
         }
