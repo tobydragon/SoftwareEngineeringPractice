b7b756f24e6814789006fe2a62f2ec034dae4572
thegoat <ruppatel11599@gmail.com>
2020-01-25 18:54:29 -0500

    withdraw tests

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -1,5 +1,8 @@
 package edu.ithaca.dragon.bank;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BankAccount {
 
     private String email;
@@ -27,18 +30,101 @@ public class BankAccount {
     }
 
     /**
+     * @throws InsufficientFundsException if withdraw amount is greater than balance
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
-    public void withdraw (double amount)  {
-        balance -= amount;
+    public void withdraw (double amount) throws InsufficientFundsException {
+        if (amount <= balance){
+            balance -= amount;
+        }
+        else {
+            throw new InsufficientFundsException("Not enough money");
+        }
+
 
     }
 
 
     public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+//        String emailRegex =  "^[a-z 0-9]+@[a-z 0-9]+\\.[a-z]{0,9}";
+//        Pattern emailPat = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
+//        Matcher matcher = emailPat.matcher(email);
+//
+//        return matcher.find();
+//        if (email.indexOf('@') == -1 && email.indexOf('.') ==-1 ){
+//            return false;
+//        }
+//        else {
+//            return true;
+//        }
+        if (email.indexOf('@') == -1 || email.indexOf('@') == 0) {
             return false;
         }
+        if (email.indexOf('.') == -1) {
+            return false;
+        }
+
+        int count = 0;
+
+        for (int i = email.indexOf('@'); i < email.length(); i++) {
+            if (email.charAt(i) == ('.')) {
+                count++;
+            }
+        }
+        if (count != 1) {
+            return false;
+        }
+
+        for (int i = 0; i < email.indexOf('@'); i++) {
+            if (email.charAt(i) == ('.')) {
+                if (email.charAt(i + 1) == ('.')) {
+                    return false;
+                }
+            }
+        }
+
+        int count3 = 0;
+        for (int j = email.indexOf('.'); j < email.length(); j++) {
+            if (email.charAt(j) != (' ')) {
+                count3++;
+            }
+        }
+
+
+        if (count3 <= 1) {
+            return false;
+        }
+
+
+        int count2=0;
+        for(int i=0; i<email.length(); i++){
+            if(email.indexOf('-') ==  email.indexOf('@')-1){
+                count2++;
+            }
+        }
+
+        if(count2 != 0){
+            return false;
+        }
+
+
+        int count4 =0;
+        int indx = email.lastIndexOf('.');
+
+        while(indx < email.length()) {
+            count4++;
+            indx++;
+        }
+        if(count4 <=1){
+            return false;
+        }
+
+
+
+
+
+
+
         else {
             return true;
         }
--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -14,11 +14,19 @@ class BankAccountTest {
     }
 
     @Test
-    void withdrawTest() {
+    void withdrawTest() throws InsufficientFundsException {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
+        assertEquals(100, bankAccount.getBalance()); // check if withdraw decreased in account
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300)); //check if withdraw amount greater than amount in bank account/ test exception
+        bankAccount.withdraw(100);
+        assertEquals(0, bankAccount.getBalance()); //check if amount of total withdraw equal to balance
+        bankAccount.withdraw(-100); //check if withdraw negative number
+        //assertEquals(0, bankAccount.getBalance()); //shouldn't equal 100
 
-        assertEquals(100, bankAccount.getBalance());
+
+
+
     }
 
     @Test
@@ -30,13 +38,13 @@ class BankAccountTest {
         assertTrue(BankAccount.isEmailValid("a1@b.com"));
         assertFalse(BankAccount.isEmailValid("@n.com"));
         assertFalse(BankAccount.isEmailValid("n.com"));
-        assertFalse(BankAccount.isEmailValid("n@Gcom"));
+        assertFalse(BankAccount.isEmailValid("n!!@Gcom"));
         assertTrue(BankAccount.isEmailValid("n@gmail.com"));
         assertTrue(BankAccount.isEmailValid("n@gyi.org"));
         assertFalse(BankAccount.isEmailValid(".n@Gcom"));
         assertFalse(BankAccount.isEmailValid(".#Gcom"));
         assertFalse(BankAccount.isEmailValid("n$@@G$$com"));
-        assertTrue(BankAccount.isEmailValid("n122@gyi.guo"));
+        assertTrue(BankAccount.isEmailValid("n122@gyi.g"));
 
 
     }
