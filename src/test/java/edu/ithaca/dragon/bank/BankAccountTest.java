0409ba00557dbc552b323f2f895f0b4ffd2f4f2b
Joel0420 <jjerez@ithaca.edu>
2020-01-26 23:48:20 -0500

    added notes to the updated test cases.

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -24,26 +24,7 @@ class BankAccountTest {
 
     @Test
     void isEmailValidTest(){
-        assertTrue(BankAccount.isEmailValid( "a@b.com"));
-        assertTrue(BankAccount.isEmailValid( "a-c@b.com"));
-        assertTrue(BankAccount.isEmailValid( "a.c@b.com"));
-        assertTrue(BankAccount.isEmailValid( "a_c@b.com"));
-        assertTrue(BankAccount.isEmailValid( "a.c@b.cc"));
-        assertTrue(BankAccount.isEmailValid( "a.c@b-archive.com"));
-        assertTrue(BankAccount.isEmailValid( "a.c@b.org"));
-        assertTrue(BankAccount.isEmailValid( "a.c@mail.com"));
 
-        
-        assertFalse(BankAccount.isEmailValid(""));
-        assertFalse(BankAccount.isEmailValid("a-@b.com"));
-        assertFalse(BankAccount.isEmailValid("a..c@b.com"));
-        assertFalse(BankAccount.isEmailValid(".a@b.com"));
-        assertFalse(BankAccount.isEmailValid("a#c@b.com"));
-        assertFalse(BankAccount.isEmailValid("a.c@b.c"));
-        assertFalse(BankAccount.isEmailValid("a.c@b#archive.com"));
-        assertFalse(BankAccount.isEmailValid("a.c@b"));
-        assertFalse(BankAccount.isEmailValid("a.c@b..com"));
-        assertFalse(BankAccount.isEmailValid("abc@def.co-"));
 
         assertTrue(BankAccount.isEmailValid("a@b.com"));
         assertTrue(BankAccount.isEmailValid("abc-d@mail.com"));
@@ -56,20 +37,35 @@ class BankAccountTest {
         assertTrue(BankAccount.isEmailValid("abc.def@mail.com"));
         assertTrue(BankAccount.isEmailValid("abc.def@mail.archive.com"));
 
+        //tests the equivalence class for no period.
         assertFalse(BankAccount.isEmailValid("a@b"));
+        //tests for no domain after @ sign
         assertFalse(BankAccount.isEmailValid("ab.com@j"));
+        //Tests for an invalid domain because it is too short
         assertFalse(BankAccount.isEmailValid("ab@j.c"));
+        //tests for a domain that is invalid because it is too long.
         assertFalse(BankAccount.isEmailValid("ab@domain.c"));
+        //Tests for a domain that comes before the @ sign and not after
         assertFalse(BankAccount.isEmailValid("ab.com@j"));
+        //Tests for an email without an @ sign
         assertFalse(BankAccount.isEmailValid("ab#c#domain.com"));
+        //Tests for an email that has a special character before the @ sign.
         assertFalse(BankAccount.isEmailValid("abc-@mail.com"));
+        //tests for a an email that has double special characters
         assertFalse(BankAccount.isEmailValid("abc..def@mail.com"));
+        //tests for an email that starts with a special character
         assertFalse(BankAccount.isEmailValid(".abc@mail.com"));
+        //tests for an email that has an invalid character.
         assertFalse(BankAccount.isEmailValid("abc$def@mail.com"));
+        //tests for an email that has invalid domain after the period because it is too short
         assertFalse(BankAccount.isEmailValid("abc.def@mail.c"));
+        //tests for an email that has an invalid character after the @ sign.
         assertFalse(BankAccount.isEmailValid("abc.def@mail#archive.com"));
+        //tests for an email that has no period after the @ sign
         assertFalse(BankAccount.isEmailValid("abc.def@mail"));
+        //tets for an email that has double special characters after the @ sign
         assertFalse(BankAccount.isEmailValid("abc.def@mail..com"));
+        //tests for a special character at the end of the email.
         assertFalse(BankAccount.isEmailValid("abc@def.co-"));
 
 
