c3eb9e232382feab1d002770d6841ac9102e4020
Justin Moczynski <jmoczynski@ithaca.edu>
2020-01-28 07:08:29 -0500

    updated tests to include border cases

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -59,19 +59,33 @@ class BankAccountTest {
         // Two dots in domain
         assertTrue(BankAccount.isEmailValid("a@b.com"));
         assertTrue(BankAccount.isEmailValid("a-b@c.com"));
+        // Border case two periods not consecutive (valid)
         assertTrue(BankAccount.isEmailValid("a.b@c.com"));
         assertTrue(BankAccount.isEmailValid("a_b@c.com"));
-        assertTrue(BankAccount.isEmailValid("a@b.cc"));
+        // Border case special character "-" before @ (valid)
         assertTrue(BankAccount.isEmailValid("a@b-c.com"));
+        // Border case special character "-" after @ (valid)
+        assertTrue(BankAccount.isEmailValid("a-b@c.com"));
+        // Border case length of email domain (valid)
+        assertTrue(BankAccount.isEmailValid("a@b.cc"));
     }
 
     @Test
     void withdrawTestUpdated() throws InsufficientFundsException{
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
-        assertThrows(IllegalArgumentException.class, ()-> bankAccount.withdraw(-100));
+        assertThrows(InsufficientFundsException.class, ()-> bankAccount.withdraw(-100));
         assertThrows(IllegalArgumentException.class, ()-> bankAccount.withdraw(300));
         bankAccount.withdraw(100);
         assertEquals(100, bankAccount.getBalance());
+
+        BankAccount bankAccount2 = new BankAccount("a@b.com", 200);
+        // Border case (3+ decimal places not allowed)
+        assertThrows(IllegalArgumentException.class, ()-> bankAccount2.withdraw(199.999));
+        // Valid border case (<=2 decimal places allowed)
+        bankAccount2.withdraw(199.99);
+        assertEquals(1.01, bankAccount2.getBalance());
+        // Invalid border case (greater amount with decimals)
+        assertThrows(InsufficientFundsException.class, ()-> bankAccount2.withdraw(200.01));
     }
 
 }
