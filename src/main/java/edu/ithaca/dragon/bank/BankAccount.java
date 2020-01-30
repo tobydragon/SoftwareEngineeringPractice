019c4f254ac8a8464a4fe8f8ef7eb1ecc847095c
Joel0420 <jjerez@ithaca.edu>
2020-01-30 18:26:39 -0500

    Full bank account class working.

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -9,9 +9,32 @@ public class BankAccount {
     private double balance;
 
     /**
+     * @throws IllegalArgumentException if amount to be withdrawn is invalid
+     * Returns a boolean
+     */
+    private boolean isAmountValid(double amount) throws IllegalArgumentException{
+        String numString = Double.toString(amount);
+        int length = numString.length();
+
+        if (length > 3){ //only runs this test if amount has more than 3 chars
+            int period = numString.lastIndexOf(".");
+            if (length > (period + 3)) throw new IllegalArgumentException("The amount you entered " + amount + " is invalid because it has more than three decimal places.");
+        }
+
+        else if (amount < 0){  //checks ifd the number is negative
+            throw new IllegalArgumentException("The amount you entered " + amount + " is invalid because it is negative");
+        }
+
+        return true; // returns true if amount is valid
+    }
+
+    /**
      * @throws IllegalArgumentException if email is invalid
      */
     public BankAccount(String email, double startingBalance){
+        if (isAmountValid(startingBalance) == false){
+            throw new IllegalArgumentException("Starting balance is an invalid balance because it is negative or has too many decimal places");
+        }
         if (isEmailValid(email)){
             this.email = email;
             this.balance = startingBalance;
@@ -30,12 +53,55 @@ public class BankAccount {
     }
 
     /**
+     * @post transfers an amount from one account to the other.
+     * parameters: bankaccount1, bankaccount2, transfer amount
+     * @throws IllegalArgumentException if deposit amount is invalid
+     *
+     */
+    public void transfer ( BankAccount bankAccountTrasferringTo, double amount) throws InsufficientFundsException, IllegalArgumentException{
+        if (isAmountValid(amount) != true){
+            throw new IllegalArgumentException ("The amount you entered: "+ amount + " is not a valid amount");
+        }
+
+        else if (balance < amount){
+            throw new InsufficientFundsException("You do not have enough money in your account to make this transfer");
+        }
+        else if (amount == 0 || amount < 0){
+            throw new IllegalArgumentException("You cannot transfer $0 or less");
+        }
+
+        else {
+            balance -= amount;
+            bankAccountTrasferringTo.balance += amount;
+        }
+    }
+
+    /**
+     * @post increases the balance by the amount deposited
+     * parameters: deposit amount
+     * @throws IllegalArgumentException if deposit amount is invalid
+     *
+     */
+
+    public void deposit (double amount) throws IllegalArgumentException {
+        if (isAmountValid(amount) == false){
+            throw new IllegalArgumentException("The amount you entered " + amount + " is invalid");
+        }
+        else {
+            balance += amount;
+        }
+    }
+
+    /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      * throws IllegalArgument if the amount is more than the balance
      * for negative numbers return the unchanged balance
      * returns the balance if the withdraw amount is less than your balance and a positive number.
      */
     public void withdraw (double amount) throws InsufficientFundsException  {
+        if (isAmountValid(amount) == false){
+            throw new IllegalArgumentException("The amount you entered " + amount + " is invalid");
+        }
 
         if (amount < .01)
             throw new IllegalArgumentException("Cannot withdraw $0 or less");
@@ -54,43 +120,111 @@ public class BankAccount {
 
     public static boolean isEmailValid(String email) {
 
-        //Checks that there is only 1 @.
-        int atIdx = email.indexOf('@');
-        int lastAtIdx = email.lastIndexOf('@');
-        if (atIdx != lastAtIdx || atIdx == -1) return false;
+        //declares a variable with the locations of the @ signs in the email.
+        int atSign = email.indexOf('@');
+        int lastAtSign = email.lastIndexOf('@');
 
-        //Checks that there is a period in the one of two spots it must be in the domain.
-        //Note that there can only be 2 or 3 characters after the last period in the domain.
-        int len = email.length();
-        int lastPeriodIdx = email.lastIndexOf('.');
-        if (lastPeriodIdx == -1) return false;
-        if (lastPeriodIdx != len - 3 && lastPeriodIdx != len - 4) return false;
+        //makes sure that an @ sign is in the email.
+        if (atSign == -1)
+            return false;
 
-        //Checks that the @ is before the last period.
-        if (atIdx>lastPeriodIdx) return false;
+        //makes sure that there is only one @ sign.
+        if (atSign != lastAtSign)
+            return false;
 
-        //Checks that all characters are valid and that the email obeys the letter after special rule.
-        int i = len - 1;
+        //Makes sure that a special character is not followed by a "@"
+        if (email.charAt(lastAtSign-1) == '.')
+            return false;
+
+        if (email.charAt(lastAtSign-1) == '_')
+            return false;
+
+        if (email.charAt(lastAtSign-1) == '-')
+            return false;
+
+        //declares a variable with the location of the last "." in the email
+        int lastPeriodSign = email.lastIndexOf('.');
+
+
+        //makes sure that there is a '.' in the email.
+        if (lastPeriodSign == -1)
+            return false;
+
+        //makes sure that there is a period after @
+        if (email.charAt(lastAtSign) < email.charAt(lastPeriodSign))
+            return false;
+
+        //makes sure that there is not two periods, dashes, or underscores back to back in the email
+        int x = 0;
+        int lengthOfEmail = email.length();
+
+        int locationOfLastPeriod = -1000;
+        int locationOfCurrentPeriod;
+
+        int locationOfLastDash = -2;
+        int locationOfCurrentDash;
+
+        int locationOfLastUnderscore = -2;
+        int locationOfCurrentUnderscore;
+
+        //makes sure the period follows a .com or .cc or .org or .edu etc...
+        if (lastPeriodSign != lengthOfEmail - 3 && lastPeriodSign != lengthOfEmail - 4)
+            return false;
+
+        while (x < (email.length()-1)){
+
+            //checks to make sure the email does not start with a period
+            if(email.charAt(x) == '.' && x == 0)
+                return false;
+            //checks for double periods
+            if (email.charAt(x) =='.' && x == (locationOfLastPeriod+1))
+                return false;
+            if(email.charAt(x) =='.')
+                locationOfLastPeriod = x;
+
+            //checks for double underscores
+            if(email.charAt(x) == '_' && x == 0)
+                return false;
+            if (email.charAt(x) == locationOfLastUnderscore)
+                return false;
+            else if(email.charAt(x) =='_')
+                locationOfLastUnderscore = x;
+
+            //checks for double dashes
+            if(email.charAt(x) == '-' && x == 0)
+                return false;
+            if (email.charAt(x) == locationOfLastDash)
+                return false;
+            else if(email.charAt(x) =='-')
+                locationOfLastDash = x;
+
+            x++;
+        }
+
+
+
+        //makes sure there is not an invalid character
+        int j = 0;
         HashSet<Character> validCharSet = getValidCharSet();
-        while (i > -1) { //going through each character in the email
+        while (j < lengthOfEmail) {
 
-            //if invalid character return false
-            if (!validCharSet.contains(email.charAt(i))) return false;
 
-            //if special character, make sure following character is not special
-            if (isSpecialChar(email.charAt(i))){
+            if (!validCharSet.contains(email.charAt(j))) return false;
+
+
+            if (isSpecialChar(email.charAt(j))){
                 //if leading or last character is special, return false
-                if (i == 0 || i == len - 1) return false;
-                //if the next character is also special return false
-                if (isSpecialChar(email.charAt(i + 1))) return false;
+                if (j == 0 || j == lengthOfEmail - 1) return false;
+                //makes sure theres no double special characters
+                if (isSpecialChar(email.charAt(j + 1))) return false;
             }
             //go back one letter
-            i--;
+            j++;
         }
-        //if no invalid characters found or special rules broken, return true
-        return true;
-    }
 
+        return true;
+
+    }
     private static boolean isSpecialChar(char c) { return c == '.' || c == '-' || c == '_' || c == '@'; }
 
     private static HashSet<Character> getValidCharSet(){
--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -8,6 +8,109 @@ import static org.junit.jupiter.api.Assertions.*;
 class BankAccountTest {
 
     @Test
+    void transferTest() throws InsufficientFundsException {
+        BankAccount bankAccount = new BankAccount("abg@g.com",100); //tests for a valid transfer
+        BankAccount bankAccount1 = new BankAccount("a@b.com",100);
+        bankAccount.transfer(bankAccount1,80);
+        assertEquals(180,bankAccount1.getBalance());
+        assertEquals(20, bankAccount.getBalance());
+
+        BankAccount bankAccount2 = new BankAccount("abg@g.com",100); // tests for a valid withdraw at the boundary of your balance
+        BankAccount bankAccount3 = new BankAccount("a@b.com",100);
+        bankAccount2.transfer(bankAccount3,100);
+        assertEquals(200,bankAccount3.getBalance());
+        assertEquals(0, bankAccount2.getBalance());
+
+        BankAccount bankAccount4 = new BankAccount("abg@g.com",30); //tests for a valid transfer at the boundary of the smallest amount
+        BankAccount bankAccount5 = new BankAccount("a@b.com",50);
+        bankAccount4.transfer(bankAccount5,0.01);
+        assertEquals(29.99,bankAccount4.getBalance());
+        assertEquals(50.01, bankAccount5.getBalance());
+
+        BankAccount bankAccount6 = new BankAccount("abg@g.com",900); //tests for an invalid transfer of 0
+        BankAccount bankAccount7 = new BankAccount("a@b.com",1000);
+        assertThrows(IllegalArgumentException.class, () -> bankAccount6.transfer(bankAccount7,0));
+        assertEquals(900,bankAccount6.getBalance());
+        assertEquals(1000, bankAccount7.getBalance());
+
+        BankAccount bankAccount8 = new BankAccount("abg@g.com",700); //tests for an invalid transfer of an amount larger than your balance
+        BankAccount bankAccount9 = new BankAccount("a@b.com",654.60);
+        assertThrows(InsufficientFundsException.class, () -> bankAccount8.transfer(bankAccount9,701));
+        assertEquals(700,bankAccount8.getBalance());
+        assertEquals(654.60, bankAccount9.getBalance());
+
+        BankAccount bankAccount10 = new BankAccount("abg@g.com",600); //tests for an invalid transfer of a negative number
+        BankAccount bankAccount11 = new BankAccount("a@b.com",600);
+        assertThrows(IllegalArgumentException.class, () -> bankAccount10.transfer(bankAccount11,-600));
+        assertEquals(600,bankAccount10.getBalance());
+        assertEquals(600, bankAccount11.getBalance());
+
+        BankAccount bankAccount12 = new BankAccount("abg@g.com",600); //tests for an invalid transfer of a negative number and an amount too large
+        BankAccount bankAccount13 = new BankAccount("a@b.com",600);
+        assertThrows(IllegalArgumentException.class, () -> bankAccount12.transfer(bankAccount13,-700));
+        assertEquals(600,bankAccount12.getBalance());
+        assertEquals(600, bankAccount13.getBalance());
+
+
+
+
+
+
+
+    }
+
+    //need to tests for deposit of 0
+
+    @Test
+    void depositTest() throws IllegalArgumentException{
+
+        BankAccount bankAccount = new BankAccount("abg@g.com",0); //tests for a valid deposit
+        bankAccount.deposit(100);
+        assertEquals(100,bankAccount.getBalance());
+
+        BankAccount bankAccount2 = new BankAccount("abg@g.com",5); //tests for a valid deposit with two decimal places
+        bankAccount2.deposit(100.50);
+        assertEquals(105.50,bankAccount2.getBalance());
+
+        BankAccount bankAccount3 = new BankAccount("abg@g.com",20); //tests for an invalid deposit with more than two decimal places
+        assertThrows(IllegalArgumentException.class, () -> bankAccount3.deposit(100.5080));
+        assertEquals(20,bankAccount3.getBalance());
+
+        BankAccount bankAccount4 = new BankAccount("abg@g.com",50); //tests for an invalid deposit with a negative number
+        assertThrows(IllegalArgumentException.class, () -> bankAccount4.deposit(-10000000));
+        assertEquals(50,bankAccount4.getBalance());
+
+        BankAccount bankAccount5 = new BankAccount("abg@g.com",75); //tests for an invalid deposit with a negative number and more than 2 decimal places
+        assertThrows(IllegalArgumentException.class, () -> bankAccount5.deposit(-10000000.9874));
+        assertEquals(75,bankAccount5.getBalance());
+
+
+
+
+    }
+
+    @Test
+    void isAmountValidTest() throws InsufficientFundsException {
+
+        BankAccount bankAccount = new BankAccount("ak3@g.com",9000);//gets a postive number with 2 decimal points. Should pass
+        bankAccount.withdraw(123.45);
+        assertEquals(8876.55, bankAccount.getBalance());
+
+        BankAccount bankAccount3 = new BankAccount("ak@g.com",45);//gets a postive number with 1 decimal point. Should pass
+        bankAccount3.withdraw(20.5);
+        assertEquals(24.5, bankAccount3.getBalance());
+
+        BankAccount bankAccount2 = new BankAccount("a@b.com", 200); //checks to make sure a positive number with 3 decimal places is invalid
+        assertThrows(IllegalArgumentException.class, () -> bankAccount2.withdraw(123.546));
+        assertEquals(200,bankAccount2.getBalance());
+
+        BankAccount bankAccount1 = new BankAccount("a@b.com", 300); //checks to make sure a negative number with 4 decimal places is invalid
+        assertThrows(IllegalArgumentException.class, () -> bankAccount1.withdraw(25.5566));
+        assertEquals(300,bankAccount1.getBalance());
+
+    }
+
+    @Test
     void getBalanceTest() {
 
         //correctly gives you the balance when you have a valid email
@@ -59,8 +162,9 @@ class BankAccountTest {
         assertEquals(590.02,bankAccount4.getBalance());
 
         //tests for an invalid withdrawal that is a fraction of a penny. Should return an illegal argument exception
-        //assertThrows(IllegalArgumentException.class, ()-> new BankAccount("a@b.com", 200).withdraw(2.005));
-
+        BankAccount bankAccount5 = new BankAccount("abc-d@mail.com", 300.50);
+        assertThrows(IllegalArgumentException.class, ()-> bankAccount4.withdraw(20.567));
+        assertEquals(300.50,bankAccount4.getBalance());
 
     }
 
@@ -116,11 +220,14 @@ class BankAccountTest {
     @Test
     void constructorTest() {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
-
         assertEquals("a@b.com", bankAccount.getEmail());
         assertEquals(200, bankAccount.getBalance());
         //check for exception thrown correctly
         assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 100));
+
+//        BankAccount bankAccount1;
+//        assertThrows(IllegalArgumentException.class ()-> bankAccount1 = new BankAccount("a@b.com", 200.98034));
+
     }
 
 }
