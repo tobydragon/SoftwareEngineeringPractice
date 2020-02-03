52c46f5972f06e29e6c1896f75b5b617beaebccd
sweeks19 <sweeks@ithaca.edu>
2020-02-03 14:43:17 -0500

    new files

---
+++ src/main/java/edu/ithaca/dragon/bank/BasicAPI.java
@@ -0,0 +1,18 @@
+package edu.ithaca.dragon.bank;
+
+//API to be used by ATMs
+public interface BasicAPI {
+
+    boolean confirmCredentials(String acctId, String password);
+
+    double checkBalance(String acctId);
+
+    void withdraw(String acctId, double amount) throws InsufficientFundsException;
+
+    void deposit(String acctId, double amount);
+
+    void transfer(String acctIdToWithdrawFrom, String acctIdToDepositTo, double amount) throws InsufficientFundsException;
+
+    String transactionHistory(String acctId);
+
+}
