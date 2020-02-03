8e38e347cddd6ba3a8a2d5d8e95b0864d0b53108
cmartano <34069445+cmartano@users.noreply.github.com>
2020-02-03 13:47:11 -0500

    Add files via upload

---
+++ src/main/java/edu/ithaca/dragon/bank/BasicAPI.java
@@ -0,0 +1,16 @@
+//API to be used by ATMs
+public interface BasicAPI {
+
+    boolean confirmCredentials(String acctId, String password);
+
+    double checkBalance(String acctId);
+
+    void withdraw(String acctId, double amount) throws InsufficientFundsException;
+
+    void deposit(String acctId, double amount);
+
+    void transfer(String acctIdToWithdrawFrom, String acctIdToDepositTo, double amount) throws InsufficientFundsException;
+
+    String transactionHistory(String acctId);
+
+}
