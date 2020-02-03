1eb0873562feb438f5dcd6fd800c993442f99294
cmartano <clmartano@gmail.com>
2020-02-03 13:48:17 -0500

    new files added in src/main

--- AdvancedAPI.java
+++
@@ -1,7 +0,0 @@
-//API to be used by Teller systems
-public interface AdvancedAPI extends BasicAPI {
-
-    public void createAccount(String acctId, double startingBalance);
-
-    public void closeAccount(String acctId);
-}
