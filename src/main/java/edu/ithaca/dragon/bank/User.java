1776b71873a8a45c21d3a620f0a7f91f398d095b
Rmelikyan <rmelikyan@ithaca.edu>
2020-02-04 14:26:17 -0500

    added User class

---
+++ src/main/java/edu/ithaca/dragon/bank/User.java
@@ -0,0 +1,9 @@
+package edu.ithaca.dragon.bank;
+
+public class User {
+    private String password;
+    private BankAccount accounts[];
+
+    //ToDo what methods might we need?
+    //Todo Do we need this class?
+}
