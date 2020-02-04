deb9a5a5c46c51fbf84590f6236970b3c590fcd8
Justin Moczynski <jmoczynski@ithaca.edu>
2020-02-04 13:01:52 -0500

    added provided interfaces for project 2

---
+++ src/main/java/edu/ithaca/dragon/bank/CentralBank.java
@@ -0,0 +1,63 @@
+package edu.ithaca.dragon.bank;
+
+import java.util.Collection;
+
+public abstract class CentralBank implements AdvancedAPI, AdminAPI {
+
+    //----------------- edu.ithaca.dragon.bank.BasicAPI methods -------------------------//
+
+    public boolean confirmCredentials(String acctId, String password) {
+        return false;
+    }
+
+    public double checkBalance(String acctId) {
+        return 0;
+    }
+
+    public void withdraw(String acctId, double amount) throws InsufficientFundsException {
+
+    }
+
+    public void deposit(String acctId, double amount) {
+
+    }
+
+    public void transfer(String acctIdToWithdrawFrom, String acctIdToDepositTo, double amount) throws InsufficientFundsException {
+
+    }
+
+    public String transactionHistory(String acctId) {
+        return null;
+    }
+
+
+    //----------------- edu.ithaca.dragon.bank.AdvancedAPI methods -------------------------//
+
+    public void createAccount(String acctId, double startingBalance) {
+
+    }
+
+    public void closeAccount(String acctId) {
+
+    }
+
+
+    //------------------ edu.ithaca.dragon.bank.AdminAPI methods -------------------------//
+
+    public double checkTotalAssets() {
+        return 0;
+    }
+
+    public Collection<String> findAcctIdsWithSuspiciousActivity() {
+        return null;
+    }
+
+    public void freezeAccount(String acctId) {
+
+    }
+
+    public void unfreezeAcct(String acctId) {
+
+    }
+
+}
