671c71167ebbb486f7094a5affe4b2ccaffba747
Vera Crabtree <vcrabtree99@gmail.com>
2020-02-05 14:37:05 -0500

    Updates to test

--- src/test/java/edu/ithaca/dragon/bank/DepositTest.java
+++ src/test/java/edu/ithaca/dragon/bank/DepositTest.java
@@ -7,29 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DepositTest {
     @Test
     void depositTest() {
-        BankAccount bankAccount = new BankAccount("a@b.com", 200);
-
-        // Negative, One to Two Decimals
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-1.01)); // border case
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-53.83));
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-9999999.9)); // border case
+        CentralBank account = new CentralBank();
 
         // Positive, One to Two Decimals
-        bankAccount.deposit(0);
-        assertEquals(200, bankAccount.getBalance()); //border case
-        bankAccount.deposit(100);
-        assertEquals(300, bankAccount.getBalance());
-        bankAccount.deposit(999);
-        assertEquals(1299, bankAccount.getBalance()); //border case
-
-        // Negative, Multiple Decimals
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-1.0000001)); // border case
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-7.48));
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-9999999.9999999)); // border case
-
-        // Positive, Multiple Decimals
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(0.000001)); // border case
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(92.498865));
-        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(9999999.999999)); //border case
+        account.deposit("a@a.com", 0);
+        assertEquals(0, account.checkBalance("a@a.com")); //border case
+        account.deposit("a@a.com", 100);
+        assertEquals(100, account.checkBalance("a@a.com"));
+        account.deposit("a@a.com", 999);
+        assertEquals(1099, account.checkBalance("a@a.com")); //border case
     }
 }
