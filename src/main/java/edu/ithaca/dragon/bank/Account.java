d6324cdd40044eeac11297caacf457f1584063a1
Stephen <stevoschool@gmail.com>
2020-02-05 14:34:27 -0500

    withdraw tests

--- src/main/java/edu/ithaca/dragon/bank/Account.java
+++ src/main/java/edu/ithaca/dragon/bank/Account.java
@@ -5,12 +5,18 @@ public abstract class Account {
     private double balance;
     private boolean frozen;
 
+
+    public Account(String IDIn, double startingBalance){
+        ID = IDIn;
+        balance = startingBalance;
+        frozen = false;
+    }
     public double getBalance(){return balance;}
     public String getID(){return ID;}
     public boolean getFrozen(){return frozen;}
     public void toggleFrozen(){frozen = !frozen;}
     public abstract void deposit(double amount);
-    public abstract void withdraw(double amount) throws InsufficientFundsException;
+    public abstract void withdraw(double amount) throws InsufficientFundsException, IllegalArgumentException;
     public abstract void transfer(Account transferTo, double amount) throws InsufficientFundsException;
 
 
--- src/main/java/edu/ithaca/dragon/bank/CheckingAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/CheckingAccount.java
@@ -1,7 +1,21 @@
 package edu.ithaca.dragon.bank;
 
 public class CheckingAccount extends Account {
+    public CheckingAccount(String ID, double balance){
+        super(ID, balance);
+    }
     public void deposit(double amount){}
-    public void withdraw(double amount) throws InsufficientFundsException{}
+    public void withdraw(double amount) throws InsufficientFundsException, IllegalArgumentException{
+        if(!CheckingAccount.isAmountValid(amount))throw new IllegalArgumentException("Not a valid Amount");
+        if(Double.compare(amount, 0.0)==0)throw new IllegalArgumentException("Cannot Withdraw zero dollars");
+
+    }
     public void transfer(Account transferTo, double amount) throws InsufficientFundsException{}
+    public static boolean isAmountValid(double amountIn){
+        if (amountIn < 0) return false;
+        double scale = Math.pow(10, 9);
+        amountIn = Math.round(amountIn*scale)/scale;
+        if(Double.compare(amountIn, Math.round(amountIn*100)/100.0)!= 0) return false;
+        else return true;
+    }
 }
