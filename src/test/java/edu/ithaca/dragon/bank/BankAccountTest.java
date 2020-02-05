aedd3c3fd042cbf49d54befbe9029f5341df0aa7
slvr611 <orion.gesslein@gmail.com>
2020-02-05 14:25:40 -0500

    Added a test

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -17,6 +17,9 @@ class BankAccountTest {
         assertEquals(1, bankAccount.getBalance());  //Edge case, positive
         bankAccount = new BankAccount("a@b.com", 0);
         assertEquals(0, bankAccount.getBalance());  //Edge case, 0
+
+        bankAccount = new BankAccount("a@b.com", 50);
+        assertEquals(50, bankAccount.getBalance());  //Edge case, 0
     }
 
     @Test
