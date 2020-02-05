3c672f91bcb6ade65adb844a934036b1e85ae852
Sean McQuillan <mcquill99@gmail.com>
2020-02-05 14:02:57 -0500

    Fixed Comments

--- src/main/java/edu/ithaca/dragon/bank/CheckingAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/CheckingAccount.java
@@ -2,8 +2,6 @@ package edu.ithaca.dragon.bank;
 
 public class CheckingAccount extends BankAccount {
     /**
-     * @param email
-     * @param startingBalance
      * @throws IllegalArgumentException if email is invalid
      */
     public CheckingAccount(String email, double startingBalance) {
