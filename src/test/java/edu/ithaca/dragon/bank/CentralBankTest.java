5a47dc79295f714d6b2fc1c3c3b11cf6642a2539
Mike Santo <aero10599@gmail.com>
2020-02-05 14:37:30 -0500

    deposit tests

--- src/test/java/edu/ithaca/dragon/bank/CentralBankTest.java
+++ src/test/java/edu/ithaca/dragon/bank/CentralBankTest.java
@@ -13,6 +13,14 @@ class CentralBankTest {
 
     @Test
     void depositTest(){
+        CentralBank newBank = new CentralBank();
+        newBank.deposit("8495739673", 50);
+        assertEquals(newBank.checkBalance("8495739673"), 50, 0.0001);
+        assertThrows(IllegalArgumentException.class, ()-> newBank.deposit("8495739673", 0));
+        assertThrows(IllegalArgumentException.class, ()-> newBank.deposit("8495739673", -5));
+        assertThrows(IllegalArgumentException.class, ()-> newBank.deposit("849573967e", 80));
+        newBank.deposit("8495739673", 80.5);
+        assertEquals(newBank.checkBalance("8495739673"), 130.50, 0.0001);
 
     }
 }
