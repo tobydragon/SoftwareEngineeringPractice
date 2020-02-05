4f406658a30b1fdebaa46de73a3eb57ab7c8a443
damelalbertos <damelalbertos@icloud.com>
2020-02-05 14:42:16 -0500

    checkBalanceTest passes. wrote implementation and wrote a quick constructor for CentralBank

--- src/test/java/edu/ithaca/dragon/bank/CentralBankTest.java
+++ src/test/java/edu/ithaca/dragon/bank/CentralBankTest.java
@@ -8,10 +8,10 @@ class CentralBankTest {
 
     @Test
     void checkBalanceTest(){
-        CentralBank testAccount = new CentralBank();
-        assertEquals(800, testAccount.checkBalance("1234"));
+        CentralBank testAccount = new CentralBank("1234", 500);
+        assertEquals(500, testAccount.checkBalance("1234"));
 
-        CentralBank testAccount2 = new CentralBank();
+        CentralBank testAccount2 = new CentralBank("4321", 50);
         assertEquals(50, testAccount2.checkBalance("4321"));
     }
 
