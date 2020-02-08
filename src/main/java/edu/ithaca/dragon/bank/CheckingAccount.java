88adf22c02422b343650a4834cbb8290f332146b
Stephen <stevoschool@gmail.com>
2020-02-08 14:18:53 -0500

    all deposit tests pass

--- src/main/java/edu/ithaca/dragon/bank/CheckingAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/CheckingAccount.java
@@ -4,7 +4,11 @@ public class CheckingAccount extends Account {
     public CheckingAccount(String ID, double balance){
         super(ID, balance);
     }
-    public void deposit(double amount){}
+    public void deposit(double amount) throws IllegalArgumentException{
+        if(!isAmountValid(amount)) throw new IllegalArgumentException("invalid amount");
+        if(amount <= 0) throw new IllegalArgumentException("amount cannot be negitive or zero");
+        balance+=amount;
+    }
     public void withdraw(double amount) throws InsufficientFundsException, IllegalArgumentException{
         if(!CheckingAccount.isAmountValid(amount))throw new IllegalArgumentException("Not a valid Amount");
         if(Double.compare(amount, 0.0)==0)throw new IllegalArgumentException("Cannot Withdraw zero dollars");
