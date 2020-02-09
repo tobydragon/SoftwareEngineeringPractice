232f38dcc055fef073aa4a3047c5ff2a8c3ffd17
cmartano <clmartano@gmail.com>
2020-02-09 16:41:21 -0500

    admin constructor and account collection added

--- src/main/java/edu/ithaca/dragon/bank/Admin.java
+++ src/main/java/edu/ithaca/dragon/bank/Admin.java
@@ -3,6 +3,12 @@ package edu.ithaca.dragon.bank;
 import java.util.Collection;
 
 public class Admin implements AdminAPI {
+    Collection<BankAccount> accounts;
+
+    public Admin(Collection<BankAccount> accounts){
+        this.accounts=accounts;
+    }
+
 
     public double calcTotalAssets() {
         return 0;
