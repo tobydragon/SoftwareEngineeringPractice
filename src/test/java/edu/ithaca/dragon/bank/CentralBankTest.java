b97900284c7847eedbccad53165dab879523054e
Eugene Kuznetsov <Azurekuz@gmail.com>
2020-02-10 12:43:38 -0500

    Made skeleton for tests.

--- src/test/java/edu/ithaca/dragon/bank/CentralBankTest.java
+++ src/test/java/edu/ithaca/dragon/bank/CentralBankTest.java
@@ -1,4 +1,55 @@
 package edu.ithaca.dragon.bank;
 
+import org.junit.jupiter.api.Test;
+
 public class CentralBankTest {
+    @Test
+    void confirmCredentialsTest(){
+
+    }
+
+    @Test
+    void checkBalanceTest(){
+
+    }
+
+    @Test
+    void withdrawTest(){
+
+    }
+
+    @Test
+    void depositTest(){
+
+    }
+
+    @Test
+    void getHistoryTest(){
+
+    }
+
+    @Test
+    void createAccountTest(){
+
+    }
+
+    @Test
+    void closeAccountTest(){
+
+    }
+
+    @Test
+    void getSuspiciousAccountsTest(){
+
+    }
+
+    @Test
+    void freezeAccountTest(){
+
+    }
+
+    @Test
+    void unfreezeAccountTest(){
+
+    }
 }
