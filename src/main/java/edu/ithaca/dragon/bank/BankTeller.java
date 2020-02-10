df106937b8188d417771116f4e969d7ed5ac51ef
Stephen <stevoschool@gmail.com>
2020-02-10 16:52:39 -0500

    added bankeTeller

---
+++ src/main/java/edu/ithaca/dragon/bank/BankTeller.java
@@ -0,0 +1,34 @@
+package edu.ithaca.dragon.bank;
+
+public class BankTeller implements AdvancedAPI {
+    public boolean confirmCredentials(String acctId, String password) {
+        return false;
+    }
+
+    public double checkBalance(String acctId) {
+        return 0;
+    }
+
+    public void withdraw(String acctId, double amount) throws InsufficientFundsException {
+
+    }
+
+    public void deposit(String acctId, double amount) {
+
+    }
+
+    public void transfer(String acctIdToWithdrawFrom, String acctIdToDepositTo, double amount) throws InsufficientFundsException {
+
+    }
+
+    public String transactionHistory(String acctId) {
+        return null;
+    }
+    
+
+    public void createAccount(String acctId, String password, double startingBalance) {
+
+    }
+
+    public void closeAccount(String acctId) {}
+}
