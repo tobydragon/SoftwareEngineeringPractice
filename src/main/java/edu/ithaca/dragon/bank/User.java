5d90a0e8a39c63a8a54f0811a235e0bf6f660330
Pax <pmeny@ithaca.edu>
2020-02-10 21:28:17 -0500

    2

--- src/main/java/edu/ithaca/dragon/bank/User.java
+++
@@ -1,9 +0,0 @@
-package edu.ithaca.dragon.bank;
-
-public class User {
-    private String password;
-    private BankAccount accounts[];
-
-    //ToDo what methods might we need?
-    //Todo Do we need this class?
-}
