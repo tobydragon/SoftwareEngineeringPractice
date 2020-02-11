a67c52e65618034b32c39eba008565a499114726
eostendarp <eostendarp@gmail.com>
2020-02-11 13:49:35 -0500

    added createAccontTest

--- src/test/java/edu/ithaca/dragon/bank/TellerTest.java
+++ src/test/java/edu/ithaca/dragon/bank/TellerTest.java
@@ -8,5 +8,16 @@ public class TellerTest {
 
     @Test
     void createAccountTest()  {
+        Teller teller = new Teller();
+        teller.createAccount("0", 0);
+
+        assertNotNull(teller.centralBank.getAccounts().get("0"));
+
+        teller.createAccount("1", 10);
+        teller.createAccount("2", 20);
+        teller.createAccount("3", 150);
+
+        assertNotNull(teller.centralBank.getAccounts().get("2"));
+
     }
 }
