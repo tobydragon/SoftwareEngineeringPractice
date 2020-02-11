5d97c7f82822066c522a9ada2a30546b069d7573
E-3P0 <elias@jwplatt.com>
2020-02-11 00:20:14 -0500

    Skeleton of both Teller class and TellerTests

---
+++ src/main/java/edu/ithaca/dragon/bank/Teller.java
@@ -0,0 +1,24 @@
+package edu.ithaca.dragon.bank;
+
+public class Teller extends ATM {
+
+    public Teller(UserArrayList userAccounts){
+        super(userAccounts); //calls ATM's constructor
+    }
+
+    public void createUserAccount(String username, String password, String email, int userID){
+
+    }
+
+    public void closeUserAccount(String userId){
+
+    }
+
+    public void createBankAccount(String acctId, double startingBalance){
+
+    }
+
+    public void closeBankAccount(String acctId){
+
+    }
+}
---
+++ src/test/java/edu/ithaca/dragon/bank/TellerTests.java
@@ -0,0 +1,14 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TellerTests {
+
+    @Test
+    public void userAccountTests(){
+        //tests opening/closing user accounts (not bank accounts)
+    }
+
+}
