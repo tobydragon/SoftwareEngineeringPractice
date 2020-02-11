438593f8ab710417b0109afcde11733d0a3bea40
Justin Moczynski <jmoczynski@ithaca.edu>
2020-02-11 13:50:31 -0500

    created SavingsAccountTest file with no relevant tests currently implemented

---
+++ src/test/java/edu/ithaca/dragon/bank/SavingsAccountTest.java
@@ -0,0 +1,29 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SavingsAccountTest {
+
+    @Test
+    void constructorTest(){
+        assertFalse(true);
+    }
+
+    @Test
+    void compoundInterestTest(){
+        assertFalse(true);
+    }
+
+    @Test
+    void withdrawTest(){
+        assertFalse(true);
+    }
+
+    @Test
+    void transferTest(){
+        assertFalse(true);
+    }
+
+}
