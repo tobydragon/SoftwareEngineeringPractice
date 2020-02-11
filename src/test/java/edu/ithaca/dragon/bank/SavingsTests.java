6943e9a70156fef5e1024b687238a0c2f32d7e34
Mike Santo <aero10599@gmail.com>
2020-02-11 02:22:31 -0500

    deposit tests fail

--- src/test/java/edu/ithaca/dragon/bank/SavingsTests.java
+++ src/test/java/edu/ithaca/dragon/bank/SavingsTests.java
@@ -8,13 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SavingsTests {
 
     @Test
+    void constructorTests(){
+
+    }
+
+    @Test
     void checkBalanceTests() throws InsufficientFundsException {
-        Savings newSavings = new Savings("1234567890", "Mike", "jdsakjh23u329", 100, 3, 50);
-        assertEquals(100, newSavings.checkBalance("1234567890"), 0.0001);
-        newSavings.withdraw("1234567890", 20);
-        assertEquals(80, newSavings.checkBalance("1234567890"),0.0001);
-        newSavings.deposit("1234567890", 40.05);
-        assertEquals(120.05, newSavings.checkBalance("1234567890"),0.0001);
+
     }
 
     @Test
@@ -24,13 +24,15 @@ public class SavingsTests {
 
     @Test
     void depositTests(){
+        Savings newSavings = new Savings("1234567890", "Mike", "jdsakjh23u329", 100, 3, 50);
+        assertEquals(100, newSavings.checkBalance("1234567890"), 0.0001);
+        newSavings.deposit("1234567890", 20);
+        assertEquals(120, newSavings.checkBalance("1234567890"),0.0001);
+        newSavings.deposit("1234567890", 40.05);
+        assertEquals(160.05, newSavings.checkBalance("1234567890"),0.0001);
 
     }
 
-    @Test
-    void transferTests(){
-
-    }
 
     @Test
     void transactionHistoryTests(){
