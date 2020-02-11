142d1f09de3f676ad875e7cc212b0c1b3deda387
IoanDascalu <idascalu@ithaca.edu>
2020-02-11 14:11:01 -0500

    Created a branch and wrote create method

--- src/main/java/edu/ithaca/dragon/bank/Teller.java
+++ src/main/java/edu/ithaca/dragon/bank/Teller.java
@@ -10,6 +10,15 @@ public class Teller extends ATM implements AdvancedAPI {
 
     }
         public void createAccount(String acctId, double startingBalance, boolean Savings){
+            if(Savings){
+                BankAccount acct= new SavingsAccount(startingBalance, acctId);
+                bank.accounts.add(acct);
+
+            }
+            else if(!Savings){
+                BankAccount acct= new CheckingAccount(startingBalance, acctId);
+                bank.accounts.add(acct);
+            }
 
         }
 
