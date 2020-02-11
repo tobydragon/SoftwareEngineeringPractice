c0ea754a6955b56827a209a7aceac5e2d2faf7e0
Aidan Saltini <alsaltini@yahoo.com>
2020-02-11 01:06:45 -0500

    added tests for findAccIdsWithSuspiciousActivity

--- src/test/java/edu/ithaca/dragon/bank/AdminTesttt.java
+++ src/test/java/edu/ithaca/dragon/bank/AdminTesttt.java
@@ -70,4 +70,30 @@ public class AdminTesttt {
         Admin admin = new Admin(bank);
         assertEquals(5500, admin.calcTotalAssets());
     }
+
+    @Test
+    void findAcctIdsWithSuspiciousActivityTest() throws InsufficientFundsException {
+        BankAccount acc1 = new CheckingAccount(1000, "acc1");
+        BankAccount acc2 = new CheckingAccount(1500, "acc2");
+        BankAccount acc3 = new SavingsAccount(3000, "acc3", .1, 3000);
+        BankAccount acc4 = new CheckingAccount(10000, "acc4");
+        CentralBank bank = new CentralBank();
+        bank.accounts.add(acc1);
+        bank.accounts.add(acc2);
+        bank.accounts.add(acc3);
+        bank.accounts.add(acc4);
+        Admin admin = new Admin(bank);
+
+        //should flag as suspicious - withdrawing more than 50% of balance
+        acc1.withdraw(1000);
+        //should not flag as suspicious - withdrawing less than 50% of balance
+        acc2.withdraw(300);
+        //should flag as suspicious - depositing more than 200% of balance
+        acc3.Deposit(1000000);
+        //should not flag as suspicious - depositing less than 200% of balance
+        acc4.Deposit(15000);
+
+        assertEquals("acc1 acc3 ", admin.findAcctIdsWithSuspiciousActivity());
+
+    }
 }
