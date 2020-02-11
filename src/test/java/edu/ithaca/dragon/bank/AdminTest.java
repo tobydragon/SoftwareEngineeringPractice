43edcd8610c2fbebc20857cdaddefab691cb697d
Aidan Saltini <alsaltini@yahoo.com>
2020-02-10 20:24:20 -0500

    calcTotalAssetsTest WIP

--- src/test/java/edu/ithaca/dragon/bank/AdminTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AdminTest.java
@@ -3,12 +3,18 @@ package edu.ithaca.dragon.bank;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AdminTest {
 
     @Test
-    void findAcctIdsWithSuspiciousActivityTest() {
+    void calcTotalAssetsTest() {
+        Collection<BankAccount> accounts = new ArrayList<BankAccount>();
+        Collection<User> users = new ArrayList<User>();
+
 
     }
 
