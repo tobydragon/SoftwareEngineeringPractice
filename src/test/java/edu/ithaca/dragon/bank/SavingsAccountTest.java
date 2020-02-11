62511189a62f239cb3acaaa297080859e2d9236c
Justin Moczynski <jmoczynski@ithaca.edu>
2020-02-11 14:15:52 -0500

    wrote some tests in SavingsAccountTest.constructorTest, added createSavivngsAccountTest

--- src/test/java/edu/ithaca/dragon/bank/SavingsAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/SavingsAccountTest.java
@@ -8,6 +8,17 @@ public class SavingsAccountTest {
 
     @Test
     void constructorTest(){
+        SavingsAccount savingsAccount1 = new SavingsAccount("a@b.com", 200, "s1"); // unit test valid equivalence class (valid email, middle case)
+        assertEquals(200, savingsAccount1.getBalance());
+        assertThrows(IllegalArgumentException.class, ()-> new SavingsAccount("a-@b.com", 200, "s2")); // integration test invalid equivalence class (invalid email, border case)
+        assertThrows(IllegalArgumentException.class, ()-> new SavingsAccount("a@b.com", -100, "s3")); // integration test invalid equivalence class (invalid amount, middle case)
+        assertThrows(IllegalArgumentException.class, ()-> new SavingsAccount("a@b.com", 0.001, "s4")); // integration test invalid equivalence class (invalid amount, border case)
+        SavingsAccount savingsAccount2 = new SavingsAccount("a@b.com", 0.01, "s5"); // integration test valid equivalence class (valid amount, border case)
+        assertEquals(0.01, savingsAccount2.getBalance());
+    }
+
+    @Test
+    void createSavingsAccountTest(){
         assertFalse(true);
     }
 
