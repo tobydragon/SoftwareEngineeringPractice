e02083a5704135fb7842cb440f65052223ea18fb
TimMarotta <timothy.marotta@yahoo.com>
2020-02-11 00:26:15 -0500

    added javadocs for Admin methods

--- src/main/java/edu/ithaca/dragon/bank/Admin.java
+++ src/main/java/edu/ithaca/dragon/bank/Admin.java
@@ -6,21 +6,39 @@ import java.util.HashMap;
 public class Admin implements AdminAPI {
     private HashMap<String, Account> accounts;
 
+    /**
+     * Calculates total balance of all accounts held by accounts HashMap
+     * @return double of balance in the bank
+     */
     @Override
     public double calcTotalAssets() {
         return 0;
     }
 
+    /**
+     * Finds accounts with suspicious activity
+     * @return list of account IDs with suspicious activity
+     */
     @Override
     public Collection<String> findAcctIdsWithSuspiciousActivity() {
         return null;
     }
 
+    /**
+     * Freezes account so its methods cannot be used
+     * @param acctId string ID of account that needs to be frozen
+     * @throws IllegalArgumentException if acctId is not in the list
+     */
     @Override
     public void freezeAccount(String acctId) throws IllegalArgumentException {
 
     }
 
+    /**
+     * Unfreezes account so its methods can be used
+     * @param acctId string ID of account that needs to be unfrozen
+     * @throws IllegalArgumentException if acctId is in the list
+     */
     @Override
     public void unfreezeAcct(String acctId) throws IllegalArgumentException {
 
---
+++ src/test/java/edu/ithaca/dragon/bank/AdminTest.java
@@ -0,0 +1,9 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AdminTest {
+
+}
