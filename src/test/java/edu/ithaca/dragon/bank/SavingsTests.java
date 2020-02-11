fa7497c8497cf6df44bf1c0af14b419284d6e749
Mike Santo <aero10599@gmail.com>
2020-02-11 04:38:19 -0500

    Savings finished

--- src/test/java/edu/ithaca/dragon/bank/SavingsTests.java
+++ src/test/java/edu/ithaca/dragon/bank/SavingsTests.java
@@ -21,11 +21,11 @@ public class SavingsTests {
     @Test
     void checkBalanceTests() {
         Savings newSavings = new Savings("1234567890", "Mike", "jdsakjh23u329", 100, 3, 50);
-        assertEquals(100, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(100, newSavings.checkBalance("1234567890"), 0.009);
         newSavings.deposit("1234567890", 20);
-        assertEquals(120, newSavings.checkBalance("1234567890"),0.001);
+        assertEquals(120, newSavings.checkBalance("1234567890"),0.009);
         newSavings.deposit("1234567890", 40.05);
-        assertEquals(160.05, newSavings.checkBalance("1234567890"),0.001);
+        assertEquals(160.05, newSavings.checkBalance("1234567890"),0.009);
         assertThrows(IllegalArgumentException.class, ()-> newSavings.checkBalance("1234567899"));
     }
 
@@ -33,31 +33,31 @@ public class SavingsTests {
     void withdrawTests() throws InsufficientFundsException {
         Savings newSavings = new Savings("1234567890", "Mike", "dg38g8qw", 100, 5, 30);
         newSavings.withdraw("1234567890", 20);
-        assertEquals(80, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(80, newSavings.checkBalance("1234567890"), 0.009);
         newSavings.withdraw("1234567890", 30);
-        assertEquals(50, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(50, newSavings.checkBalance("1234567890"), 0.009);
         assertThrows(IllegalArgumentException.class, ()-> newSavings.withdraw("1234567890", 30.01));
-        assertEquals(50, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(50, newSavings.checkBalance("1234567890"), 0.009);
         assertThrows(IllegalArgumentException.class, ()-> newSavings.withdraw("1234567880", 25));
-        assertEquals(50, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(50, newSavings.checkBalance("1234567890"), 0.009);
         assertThrows(IllegalArgumentException.class, ()-> newSavings.withdraw("1234567890", 0));
-        assertEquals(50, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(50, newSavings.checkBalance("1234567890"), 0.009);
         assertThrows(IllegalArgumentException.class, ()-> newSavings.withdraw("1234567880", -25));
-        assertEquals(50, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(50, newSavings.checkBalance("1234567890"), 0.009);
         newSavings.withdraw("1234567890", 30);
         assertEquals(20, newSavings.checkBalance("1234567890"));
         assertThrows(InsufficientFundsException.class, ()-> newSavings.withdraw("1234567890", 20.01));
-        assertEquals(20, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(20, newSavings.checkBalance("1234567890"), 0.009);
     }
 
     @Test
     void depositTests(){
         Savings newSavings = new Savings("1234567890", "Mike", "jdsakjh23u329", 100, 3, 50);
-        assertEquals(100, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(100, newSavings.checkBalance("1234567890"), 0.009);
         newSavings.deposit("1234567890", 20);
-        assertEquals(120, newSavings.checkBalance("1234567890"),0.001);
+        assertEquals(120, newSavings.checkBalance("1234567890"),0.009);
         newSavings.deposit("1234567890", 40.05);
-        assertEquals(160.05, newSavings.checkBalance("1234567890"),0.001);
+        assertEquals(160.05, newSavings.checkBalance("1234567890"),0.009);
         assertThrows(IllegalArgumentException.class, ()-> newSavings.deposit("1234567899", 10));
         assertThrows(IllegalArgumentException.class, ()-> newSavings.deposit("1234567890", 0));
         assertThrows(IllegalArgumentException.class, ()-> newSavings.deposit("1234567890", -10));
@@ -80,9 +80,9 @@ public class SavingsTests {
     void compoundInterestTests(){
         Savings newSavings = new Savings("1234567890", "Mike", "fduiewh9uf", 200, 3, 40);
         newSavings.compoundInterest("1234567890");
-        assertEquals(206, newSavings.checkBalance("1234567890"), 0.001);
+        assertEquals(206, newSavings.checkBalance("1234567890"), 0.009);
         newSavings.compoundInterest("1234567890");
-        assertEquals(212.18, newSavings.checkBalance("1234567890"),0.001);
+        assertEquals(212.18, newSavings.checkBalance("1234567890"),0.009);
         newSavings.compoundInterest("1234567890");
         assertEquals(218.54, newSavings.checkBalance("1234567890"),0.009);
         assertThrows(IllegalArgumentException.class, ()-> newSavings.compoundInterest("1234567899"));
