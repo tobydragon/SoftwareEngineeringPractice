f12525a07a94efe59461a0ee360c3e758ee72d88
Mike Santo <aero10599@gmail.com>
2020-02-10 19:46:08 -0500

    Savings class created

---
+++ src/main/java/edu/ithaca/dragon/bank/Savings.java
@@ -0,0 +1,45 @@
+package edu.ithaca.dragon.bank;
+
+import java.util.List;
+
+public class Savings implements Account{
+
+    private String acctId;
+    private String name;
+    private String password;
+    private double balance;
+    private boolean frozen;
+    private List<String []> history;
+    private double compoundedInterest;
+    private double interestRate;
+    private double maxWithdrawal;
+
+    public Savings(int acctIdIn, String nameIn, String passwordIn, double startingBalance, double interestRateIn, double maxWithdrawalIn){
+        //todo
+    }
+
+    public double checkBalance(String acctId){
+        return 0;
+    }
+
+    public void withdraw(String acctId, double amount) throws InsufficientFundsException{
+        //todo
+    }
+
+    public void deposit(String acctId, double amount){
+        //todo
+    }
+
+    public void transfer(String acctIdToWithdrawFrom, String acctIdToDepositTo, double amount) throws InsufficientFundsException{
+        //todo
+    }
+
+    public String transactionHistory(String acctId){
+        return null;
+    }
+
+    public void compoundInterest(String acctId){
+        //todo
+    }
+
+}
