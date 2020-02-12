355baea2bcc2d5012da48bdff45f232b6ca1df2d
TimMarotta <timothy.marotta@yahoo.com>
2020-02-12 09:07:56 -0500

    created tests for Admin.unfreezeAcct

--- src/test/java/edu/ithaca/dragon/bank/AdminTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AdminTest.java
@@ -63,4 +63,34 @@ public class AdminTest {
         assertTrue(savingsTester.getFrozenStatus());
     }
 
+    @Test
+    void unfreezeAccountTest(){
+        Admin admin = new Admin();
+        //unfreeze account with zero
+        admin.createCheckingForTeller("1452569405", "Toby Dragon", "password", 0);
+        admin.freezeAccount("1452569405");
+        admin.unfreezeAcct("1452569405");
+        Account checkingTester = admin.getAccount("1452569405");
+        assertFalse(checkingTester.getFrozenStatus());
+
+        //unfreeze unfrozen account with zero
+        admin.createSavingsForTeller("1029305967", "Doug Turnbull", "localify", 0, 5, 100);
+        admin.unfreezeAcct("1029305967");
+        Account savingsTester = admin.getAccount("1029305967");
+        assertFalse(savingsTester.getFrozenStatus());
+
+        //unfreeze account with balance
+        admin.createCheckingForTeller("1452048605", "Ali Erkan", "networks", 100);
+        admin.freezeAccount("1452569405");
+        admin.unfreezeAcct("1452569405");
+        checkingTester = admin.getAccount("1452048605");
+        assertFalse(checkingTester.getFrozenStatus());
+
+        //unfreeze unfrozen account with balance
+        admin.createSavingsForTeller("1021059277", "Sharon Stansfield", "virtual", 200, 5, 100);
+        admin.unfreezeAcct("1021059277");
+        savingsTester = admin.getAccount("1021059277");
+        assertFalse(savingsTester.getFrozenStatus());
+    }
+
 }
