7b4ee1847d384749783939fc3e083954c61a70ad
elonskolnik <42852166+elonskolnik@users.noreply.github.com>
2020-02-13 15:43:03 -0500

    Added tests for atm deposit

--- src/test/java/edu/ithaca/dragon/bank/ATMTest.java
+++ src/test/java/edu/ithaca/dragon/bank/ATMTest.java
@@ -134,7 +134,66 @@ public class ATMTest {
 
     @Test
     void depositTest() throws IllegalArgumentException, AcctFrozenException{
+        Admin admin = new Admin();
+        ATM finalAtm = new ATM();
 
+        //valid checking acct positive withdrawal
+        admin.createCheckingForTeller("1111111111", "Bob Jo", "dog223", 500);
+        finalAtm.deposit("1111111111", 200.75);
+        assertEquals(299.25, finalAtm.checkBalance("1111111111"));
+
+        //valid checking acct left with 0
+        finalAtm.deposit("1111111111", 299.25);
+        assertEquals(0, finalAtm.checkBalance("1111111111"));
+
+        //valid checking acct negative withdrawal
+        assertThrows(IllegalArgumentException.class, ()-> finalAtm.deposit("2222222222", -35));
+
+        //valid checking acct three decimal points
+        assertThrows(IllegalArgumentException.class, ()-> finalAtm.deposit("2222222222", 211.532));
+
+        //frozen checking acct
+        admin.freezeAccount("2222222222");
+        assertThrows(AcctFrozenException.class, ()-> finalAtm.deposit("2222222222", 35));
+
+        //checking acct after unfrozen
+        admin.unfreezeAcct("2222222222");
+        finalAtm.deposit("2222222222", 40);
+        assertEquals(260, finalAtm.checkBalance("2222222222"));
+
+        //valid savings acct positive withdrawal
+        admin.createSavingsForTeller("3333333333", "Rob Jo", "dog443", 300, .1, 600);
+        finalAtm.deposit("1111111111", 175);
+        assertEquals(125, finalAtm.checkBalance("3333333333"));
+
+        //valid savings acct left with 0
+        finalAtm.deposit("3333333333", 125);
+        assertEquals(0, finalAtm.checkBalance("3333333333"));
+
+        //valid savings acct overdraw
+        admin.createSavingsForTeller("4444444444", "Goo Boo", "dog64", 1000, .2, 600);
+        assertThrows(InsufficientFundsException.class, ()-> finalAtm.deposit("4444444444", 1001));
+
+        //valid savings acct negative withdrawal
+        assertThrows(IllegalArgumentException.class, ()-> finalAtm.deposit("4444444444", -35));
+
+        //valid savings acct three decimal points
+        assertThrows(IllegalArgumentException.class, ()-> finalAtm.deposit("4444444444", 35.632));
+
+        //frozen savings acct
+        admin.freezeAccount("4444444444");
+        assertThrows(AcctFrozenException.class, ()-> finalAtm.deposit("4444444444", 50));
+
+        //savings acct after unfrozen
+        admin.unfreezeAcct("4444444444");
+        finalAtm.deposit("4444444444", 400);
+        assertEquals(600, finalAtm.checkBalance("4444444444"));
+
+        //savings acct over withdrawal max
+        assertThrows(IllegalArgumentException.class, ()-> finalAtm.deposit("4444444444", 400));
+
+        //invalid acct
+        assertThrows(IllegalArgumentException.class, ()-> finalAtm.deposit("23535353", 250));
     }
 
     @Test
