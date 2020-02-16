753c55afb1e29a1c3ac863397bbc9c0babce55bb
JoshHayden <jhayden@ithaca.edu>
2020-02-16 12:47:41 -0500

    Added confirmCredentials test

--- src/test/java/edu/ithaca/dragon/bank/ATMTest.java
+++ src/test/java/edu/ithaca/dragon/bank/ATMTest.java
@@ -87,6 +87,58 @@ public class ATMTest {
         assertThrows(IllegalArgumentException.class, () -> a.transfer("abc", "help", 100));
     }
 
+    @Test
+    void confirmCredentialsTest(){
+        CentralBank bank = new CentralBank();
+        bank.getAccounts().put("abc", new CheckingAccount(100, "abc", "password"));
+        bank.getAccounts().put("def", new CheckingAccount(100, "def", "123ABcD"));
+        ATM a = new ATM(bank);
+
+        //Checking basic case for true and false
+        assertTrue(a.confirmCredentials("abc", "password"));
+        assertFalse(a.confirmCredentials("ghi", "notOnTheList"));
+
+        //Checking it distinguishes capitals
+        assertTrue(a.confirmCredentials("def", "123ABcD"));
+        assertFalse(a.confirmCredentials("def", "123abcd"));
+        assertFalse(a.confirmCredentials("def", "123ABCD"));
+
+        //Check that password matches account
+        assertFalse(a.confirmCredentials("abc", "123ABcD"));
+        assertFalse(a.confirmCredentials("def", "password"));
+
+        //Check that one char missing from password fails
+        assertFalse(a.confirmCredentials("abc", "assword"));
+        assertFalse(a.confirmCredentials("abc", "passwor"));
+        assertFalse(a.confirmCredentials("abc", "passord"));
+
+        //Check that one char missing from id fails
+        assertFalse(a.confirmCredentials("bc", "password"));
+        assertFalse(a.confirmCredentials("ac", "password"));
+        assertFalse(a.confirmCredentials("ab", "password"));
+
+        //Check that one extra char in password fails
+        assertFalse(a.confirmCredentials("abc", "password1"));
+        assertFalse(a.confirmCredentials("abc", "pass1word"));
+        assertFalse(a.confirmCredentials("abc", "1password"));
+
+        //Check that one extra char in id fails
+        assertFalse(a.confirmCredentials("abcd", "password"));
+        assertFalse(a.confirmCredentials("abdc", "password"));
+        assertFalse(a.confirmCredentials("dabc", "password"));
+
+        //Check that one different character in password fails
+        assertFalse(a.confirmCredentials("abc", "dassword"));
+        assertFalse(a.confirmCredentials("abc", "passmord"));
+        assertFalse(a.confirmCredentials("abc", "passwork"));
+
+        //Check that one different character in id fails
+        assertFalse(a.confirmCredentials("dbc", "password"));
+        assertFalse(a.confirmCredentials("ajc", "password"));
+        assertFalse(a.confirmCredentials("ab1", "password"));
+
+    }
+
 
 
 }
