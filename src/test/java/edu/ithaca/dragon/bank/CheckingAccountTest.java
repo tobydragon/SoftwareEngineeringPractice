81df9453becc47635ae0f203bae20e92bbae73b8
Justin Moczynski <jmoczynski@ithaca.edu>
2020-02-17 13:18:25 -0500

    adding tests for CheckingAccount in CheckingAccountTest

---
+++ src/test/java/edu/ithaca/dragon/bank/CheckingAccountTest.java
@@ -0,0 +1,34 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CheckingAccountTest {
+
+    @Test
+    void constructorTest(){
+
+    }
+
+    @Test
+    void createCheckingAccountTest(){
+
+    }
+
+    @Test
+    void withdrawTest(){
+
+    }
+
+    @Test
+    void depositTest(){
+
+    }
+
+    @Test
+    void transferTest(){
+
+    }
+
+}
