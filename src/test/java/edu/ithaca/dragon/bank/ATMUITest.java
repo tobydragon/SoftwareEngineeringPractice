778b47b29a41571a26c61ca8c400fecd3e1f4df5
AidanD-boot <adebolt@ithaca.edu>
2020-02-18 10:47:03 -0500

    Added start of ATMUITest

--- src/test/java/edu/ithaca/dragon/bank/ATMUITest.java
+++ src/test/java/edu/ithaca/dragon/bank/ATMUITest.java
@@ -1,4 +1,9 @@
 package edu.ithaca.dragon.bank;
 
 public class ATMUITest {
+    public static void main(String[] args) {
+        CentralBank testBank = new CentralBank("NewBank");
+        BasicAPI testAPI = testBank;
+        ATMUI testAtmUI = new ATMUI();
+    }
 }
