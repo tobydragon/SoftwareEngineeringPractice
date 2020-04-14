849944238259c36118a5d244182787c6630eb4f5
Toby Dragon <dr.toby.dragon@gmail.com>
2020-04-14 12:59:00 -0400

    added a failing test

--- src/test/java/edu/ithaca/dragon/bank/CentralBankTest.java
+++ src/test/java/edu/ithaca/dragon/bank/CentralBankTest.java
@@ -5,13 +5,14 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CentralBankTest {
 
     @Test
-    void getCustomerCount() throws IOException {
+    void numberOfAccountsTest() throws IOException {
         // Code that generated the original file
 //        CentralBank testBank = new CentralBank();
 //        testBank.addAccounts(JsonUtil.listFromJsonFile("src/test/resources/listFromJsonFileTest.json", BankAccount.class));
@@ -19,5 +20,15 @@ class CentralBankTest {
 
         CentralBank testBank = JsonUtil.fromJsonFile("src/test/resources/centralBankTest.json", CentralBank.class);
         Assert.assertEquals(4, testBank.numberOfAccounts());
+        testBank.addAccounts(Arrays.asList(new BankAccount("h@i.com", 100), new BankAccount("j@k.com", 200)));
+        Assert.assertEquals(6, testBank.numberOfAccounts());
+    }
+
+    @Test
+    void numberOfAccountsTest2() throws IOException {
+        CentralBank testBank = JsonUtil.fromJsonFile("src/test/resources/centralBankTest.json", CentralBank.class);
+        Assert.assertEquals(4, testBank.numberOfAccounts());
+        testBank.addAccounts(Arrays.asList(new BankAccount("h@i.com", 100), new BankAccount("a@b.com", 200)));
+        Assert.assertEquals(6, testBank.numberOfAccounts());
     }
 }
